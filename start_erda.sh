6194b8731f35bc9346970e050325cd020f61b9ea
MartinFrølund <martin.frolund@gmail.com>
2021-09-09 08:13:51 +0200

    Edited start_erda script to start up ssh agent

--- start_erda.sh
+++ start_erda.sh
@@ -1,3 +1,7 @@
+# Startup ssh agent
+eval "$(ssh-agent -s)"
+ssh-add ./work/.ssh/id_rsa
+
 # Go to folder and branch
 cd work/MasterThesis2021_src/
 git checkout lorentz-block-experiment-v1-31.08.2021
