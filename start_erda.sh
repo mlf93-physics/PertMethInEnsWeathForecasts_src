196161cd06b950ba5aeac57c517cca804cb4814b
MartinFrølund <martin.frolund@gmail.com>
2021-09-09 08:09:22 +0200

    Added start_erda.sh script to do start up commands of git and conda on ERDA.

---
+++ start_erda.sh
@@ -0,0 +1,9 @@
+# Go to folder and branch
+cd work/MasterThesis2021_src/
+git checkout lorentz-block-experiment-v1-31.08.2021
+git pull
+
+# Conda env stuff
+conda activate python3
+conda env update --file environment.yml
+
