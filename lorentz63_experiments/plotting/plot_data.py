88625057fc64f4631934531da84a6d83f89d847d
MartinFrølund <martin.frolund@gmail.com>
2021-10-01 16:26:13 +0200

    Solved error with cmap in quiver 3d plot of l63 model and e vector dist.

--- lorentz63_experiments/plotting/plot_data.py
+++ lorentz63_experiments/plotting/plot_data.py
@@ -101,9 +101,11 @@ def plot_normal_mode_dist(args):
     ax1 = plt.axes(projection="3d")
 
     max_e_value = np.max(e_values.real)
+    min_e_value = np.min(e_values.real)
 
+    # Prepare cmap and norm
     cmap, norm = g_plt_utils.get_cmap_distributed_around_zero(
-        vmin=-max_e_value, vmax=max_e_value
+        vmin=min_e_value, vmax=max_e_value
     )
     # Plot attractor
     plot_attractor(args, ax=ax1)
@@ -114,7 +116,7 @@ def plot_normal_mode_dist(args):
         u_profiles[1, :],
         u_profiles[2, :],
         c=e_values.real,
-        norm=norm,  # mpl_colors.Normalize(-max_e_value, max_e_value),
+        norm=norm,
         cmap=cmap,
     )
     ax1.set_title(
@@ -125,7 +127,6 @@ def plot_normal_mode_dist(args):
     fig1.colorbar(scatter_plot)
 
     fig2 = plt.figure()
-    cmap = "coolwarm"
     ax2 = plt.axes(projection="3d")
     plot_attractor(args, ax=ax2)
     qplot = ax2.quiver(
@@ -135,7 +136,7 @@ def plot_normal_mode_dist(args):
         e_vector_matrix[0, :].real,
         e_vector_matrix[1, :].real,
         e_vector_matrix[2, :].real,
-        norm=mpl_colors.Normalize(-max_e_value, max_e_value),
+        norm=norm,
         cmap=cmap,
         normalize=True,
         length=2,
@@ -146,24 +147,14 @@ def plot_normal_mode_dist(args):
         + f", r={ref_header_dict['r_const']}, b={ref_header_dict['b_const']:.2f}"
     )
 
-    qplot.set_array(np.linspace(-max_e_value, max_e_value, 100))
-    # Prepare quiver colors
-    colors = e_values.real
-    # Flatten and normalize
-    colors = (colors.ravel()) / (0.5 * colors.ptp())
-
-    # Repeat for each body line and two head lines
-    colors = np.concatenate((colors, np.repeat(colors, 2)))
-    # repeated_mask = np.concatenate((mask.ravel(), np.repeat(mask.ravel(), 2)))
-    # Colormap
-    colors = getattr(plt.cm, cmap)(colors)
-    qplot.set_edgecolor(colors)
-    qplot.set_facecolor(colors)
+    # Set quiver colors
+    qplot.set_array(np.concatenate((e_values.real, np.repeat(e_values.real, 2))))
+    # Set colorbar
     fig2.colorbar(qplot)
 
     if args["save_plot"]:
         out_path = pl.Path(
-            "../../thesis/figures/" + "lorentz63_experiments/normal_mode_perturbations/"
+            "../../thesis/figures/lorentz63_experiments/normal_mode_perturbations/"
         )
         name1 = f"nm_perturbations_e_value_dist_n{args['n_profiles']}"
         name2 = f"nm_perturbations_e_vector_dist_n{args['n_profiles']}"
