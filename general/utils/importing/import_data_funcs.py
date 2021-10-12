Moved saving or showing the figure into a seperate function in plot_utils. Added shell_cutoff to the plot arguments. Fixed bug in the normal modes calculation. Added plot of the perturbation trajectory energy spectrum (i.e. without subtracting reference velocities (for the shell model only).

general/utils/importing/import_data_funcs.py
@@ -253,11 +253,11 @@ def import_ref_data(args=None):
 
 
 def import_perturbation_velocities(
-    args=None, search_pattern="*.csv", shell_cutoff: int = None
+    args=None, search_pattern="*.csv", raw_perturbations=False
 ):
     if MODEL != Models.SHELL_MODEL:
-        if shell_cutoff is not None:
-            raise g_exceptions.InvalidFuncArgument(argument="shell_cutoff")
+        if args["shell_cutoff"] is not None:
+            raise g_exceptions.InvalidRuntimeArgument(argument="shell_cutoff")
 
     u_stores = []
 
@@ -349,17 +349,21 @@ def import_perturbation_velocities(
             )
             counter += 1
 
-        if shell_cutoff is not None:
+        # If raw_perturbations is True, the reference data is not subtracted from perturbation
+        if args["shell_cutoff"] is not None:
             # Calculate error array
-            # Add +2 to shell_cutoff since first entry is time, and shell_cutoff
+            # Add +2 to args["shell_cutoff"] since first entry is time, and args["shell_cutoff"]
             # starts from 0
             u_stores.append(
-                perturb_data_in[:, 1 : (shell_cutoff + 2)]
-                - ref_data_in[:, 1 : (shell_cutoff + 2)]
+                perturb_data_in[:, 1 : (args["shell_cutoff"] + 2)]
+                - (not raw_perturbations)
+                * ref_data_in[:, 1 : (args["shell_cutoff"] + 2)]
             )
         else:
             # Calculate error array
-            u_stores.append(perturb_data_in[:, 1:] - ref_data_in[:, 1:])
+            u_stores.append(
+                perturb_data_in[:, 1:] - (not raw_perturbations) * ref_data_in[:, 1:]
+            )
         # If perturb positions are the same for all perturbations, return
         # ref_data_in too
         if np.unique(perturb_time_pos_list).size == 1:

shell_model_experiments/plotting/plot_data.py
@@ -54,7 +54,7 @@ def plot_shells_vs_time(k_vectors_to_plot=None):
     # plt.figlegend(handles, labels, loc='center right', bbox_to_anchor=(1.0, 0.5))
 
 
-def plot_energy_spectrum(u_store, header_dict, axes=None, omit=None):
+def plot_energy_spectrum(u_data, header_dict, axes=None):
 
     if axes is None:
         fig = plt.figure()
@@ -63,8 +63,8 @@ def plot_energy_spectrum(u_store, header_dict, axes=None, omit=None):
     # Calculate mean energy
     mean_energy = np.mean(
         (
-            u_store[-u_store.shape[0] // 4 :]
-            * np.conj(u_store[-u_store.shape[0] // 4 :])
+            u_data[-u_data.shape[0] // 4 :, :]
+            * np.conj(u_data[-u_data.shape[0] // 4 :, :])
         ).real,
         axis=0,
     )
@@ -83,7 +83,7 @@ def plot_energy_spectrum(u_store, header_dict, axes=None, omit=None):
     axes.set_ylabel("Energy")
     # Limit y axis if necessary
     if np.log(np.min(mean_energy)) < -15:
-        axes.set_ylim(10e-15, 1)
+        axes.set_ylim(1e-15, 10)
 
     # Title setup
     title = g_plt_utils.generate_title(
@@ -580,6 +580,22 @@ def plot_eigen_vector_comparison(args=None):
     )
 
 
+def plot_pert_traject_energy_spectrum(args):
+
+    (
+        u_stores,
+        perturb_time_pos_list,
+        perturb_time_pos_list_legend,
+        header_dict,
+        _,
+    ) = g_import.import_perturbation_velocities(args, raw_perturbations=True)
+
+    axes = plt.axes()
+
+    for u_data in u_stores:
+        plot_energy_spectrum(u_data, header_dict, axes=axes)
+
+
 def plot_error_energy_spectrum_vs_time_2D(args=None):
 
     (
@@ -810,11 +826,14 @@ if __name__ == "__main__":
     if "energy_plots" in args["plot_type"]:
         plots_related_to_energy(args=args)
 
+    if "pert_traj_energy_spectrum" in args["plot_type"]:
+        plot_pert_traject_energy_spectrum(args)
+
     if "error_norm" in args["plot_type"]:
         if args["datapath"] is None:
             print("No path specified to analyse error norms.")
         else:
-            g_plt_data.plot_error_norm_vs_time(args=args, shell_cutoff=10)
+            g_plt_data.plot_error_norm_vs_time(args=args)
 
     if "error_spectrum_vs_time" in args["plot_type"]:
         plot_error_energy_spectrum_vs_time_2D(args=args)
@@ -840,6 +859,4 @@ if __name__ == "__main__":
     if "error_vector_spectrum" in args["plot_type"]:
         plot_error_vector_spectrum(args=args)
 
-    if not args["noplot"]:
-        plt.tight_layout()
-        plt.show()
+    g_plt_utils.save_or_show_plot(args)