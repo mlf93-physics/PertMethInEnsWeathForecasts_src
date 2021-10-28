Bug fixes of the EOF analysis. Sorting of eigenvalues according to the eigenvalues of the covariance matrix. Fixed error with rd and nm perturbation runs in the compare_perturbations_runner.py.

general/analyses/breed_vector_eof_analysis.py
@@ -51,6 +51,10 @@ def calc_bv_eof_vectors(breed_vectors: np.ndarray, n_eof_vectors: int) -> np.nda
 
     # Calculate eigenvalues and -vectors
     e_values, e_vectors = np.linalg.eig(cov_matrix)
+    # Take absolute in order to avoid negative variances
+    e_values = np.abs(e_values)
+
+    sort_indices = np.argsort(e_values, axis=1)[:, ::-1]
 
     # Project e vectors onto the breed vectors to get the EOF vectors
     eof_vectors: np.ndarray((n_units, params.sdim, n_vectors)) = (
@@ -66,17 +70,28 @@ def calc_bv_eof_vectors(breed_vectors: np.ndarray, n_eof_vectors: int) -> np.nda
         )
     ).real
 
+    # Get sorted vectors
+    eof_vectors = eof_vectors[np.arange(n_units)[:, np.newaxis], :, sort_indices]
+    eof_vectors = np.transpose(eof_vectors, axes=(0, 2, 1))
+
+    # Filter out unused eof's
     eof_vectors = eof_vectors[:, :, :n_eof_vectors]
 
     return eof_vectors
 
 
 if __name__ == "__main__":
+    cfg.init_licence()
     # Get arguments
     mult_pert_arg_setup = a_parsers.MultiPerturbationArgSetup()
     mult_pert_arg_setup.setup_parser()
     args = mult_pert_arg_setup.args
     # Get BVs
-    perturb_vectors, perturb_header_dicts = pt_import.import_perturb_vectors(args)
+    (
+        vector_units,
+        u_init_profiles,
+        eval_pos,
+        perturb_header_dicts,
+    ) = pt_import.import_perturb_vectors(args)
 
-    calc_bv_eof_vectors(perturb_vectors)
+    calc_bv_eof_vectors(vector_units, 2)

lorentz63_experiments/perturbations/normal_modes.py
@@ -39,13 +39,11 @@ def find_normal_modes(u_init_profiles, args, dev_plot_active=False, n_profiles=N
     # Initialise the jacobian
     j_matrix = init_jacobian(args)
 
-    # Need to be done for Numba to work
-    u_init_profiles = np.copy(u_init_profiles)
-
     # Perform calculation for all u_profiles
     for i in range(n_profiles):
         # Calculate the Jacobian matrix
-        calc_jacobian(j_matrix, u_init_profiles[:, i], r_const=args["r_const"])
+        # np.copy is needed for Numba to work
+        calc_jacobian(j_matrix, np.copy(u_init_profiles[:, i]), r_const=args["r_const"])
 
         e_values, e_vectors = np.linalg.eig(j_matrix)
 