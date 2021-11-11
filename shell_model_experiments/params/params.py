83fd2ae4ea151ef8b55b7b66bb688ddcad3660c7
MartinFrølund <martin.frolund@gmail.com>
2021-11-11 16:39:10 +0100

    Working on implementing numba compiled param class.

--- shell_model_experiments/params/params.py
+++ shell_model_experiments/params/params.py
@@ -1,103 +1,171 @@
+import sys
+
+sys.path.append("..")
 import math
 import numpy as np
-
-#### Initialise sabra model constants ####
-epsilon = 0.5
-lambda_const = 2
-dt = 1e-7
-sample_rate = 1 / 1000
-bd_size = 2
-n_forcing = 0
-u0 = 1
-
-# Define some conversion shortcuts
-tts = sample_rate / dt
-stt = dt / sample_rate
-
-# Define types
-dtype = np.complex128
-
-# Define factors to be used in the derivative calculation
-factor2 = -epsilon / lambda_const
-factor3 = (1 - epsilon) / lambda_const ** 2
+import numba as nb
+from numba.experimental import jitclass
+from general.utils.module_import.type_import import *
 
 
-# Define u_slice
-u_slice = np.s_[bd_size:-bd_size:1]
+@jitclass(
+    [
+        ("epsilon", nb.types.float64),
+        ("lambda_const", nb.types.float64),
+        ("dt", nb.types.float64),
+        ("sample_rate", nb.types.float64),
+        ("bd_size", nb.types.int16),
+        ("n_forcing", nb.types.int16),
+        ("u0", nb.types.float64),
+        ("seeked_error_norm", nb.types.float64),
+        ("sdim", nb.types.int32),
+        ("tts", nb.types.float64),
+        ("stt", nb.types.float64),
+        ("factor2", nb.types.float64),
+        ("factor3", nb.types.float64),
+        ("k_vec_temp", nb.types.Array(nb.types.float64, 1, "C", readonly=True)),
+        ("pre_factor", nb.types.Array(nb.types.complex128, 1, "C", readonly=True)),
+        ("hel_pre_factor", nb.types.Array(nb.types.float64, 1, "C", readonly=True)),
+        ("du_array", nb.types.Array(nb.types.complex128, 1, "C", readonly=False)),
+        ("initial_k_vec", nb.types.Array(nb.types.float64, 1, "C", readonly=True)),
+    ],
+)
+class Params:
+    def __init__(self):
+        self.epsilon: float = 0.5
+        self.lambda_const: float = 2
+        self.dt: float = 1e-7
+        self.sample_rate: float = 1 / 1000
+        self.bd_size: int = 2
+        self.n_forcing: int = 0
+        self.u0: float = 1
+        self.seeked_error_norm: float = 1e-4
+        self.sdim: int = 20
+
+        # Define conversion factors
+        self.tts: float = self.sample_rate / self.dt
+        self.stt: float = self.dt / self.sample_rate
+
+        # Define factors for derivative calculation
+        self.factor2: float = -self.epsilon / self.lambda_const
+        self.factor3: float = (1 - self.epsilon) / self.lambda_const ** 2
+
+        # Define arrays
+        self.k_vec_temp: np.ndarray = np.array(
+            [self.lambda_const ** (n + 1) for n in range(self.sdim)],
+            dtype=np.float64,
+        )
+        self.pre_factor: np.ndarray = 1j * self.k_vec_temp
+        self.hel_pre_factor: np.ndarray = (-1) ** (
+            np.arange(1, self.sdim + 1)
+        ) * self.k_vec_temp
+        self.du_array: np.ndarray = np.zeros(
+            self.sdim + 2 * self.bd_size, dtype=np.complex128
+        )
+        self.initial_k_vec: np.ndarray = self.k_vec_temp ** (-1 / 3)
+
+    def initialise_arrays(self):
+
+        # Define k vector indices array
+        self.k_vec_temp = np.array(
+            [self.lambda_const ** (n + 1) for n in range(self.sdim)],
+            dtype=np.float64,
+        )
+
+        # Define pre_factor for derivative calculation
+        self.pre_factor = 1j * self.k_vec_temp
+
+        # Helicity pre factor
+        self.hel_pre_factor = (-1) ** (np.arange(1, self.sdim + 1)) * self.k_vec_temp
+
+        # Define du array to store derivative
+        self.du_array = np.zeros(self.sdim + 2 * self.bd_size, dtype=np.complex128)
+
+        # Calculate initial k and u profile. Put in zeros at the boundaries
+        self.initial_k_vec = self.k_vec_temp ** (-1 / 3)
 
 
-#### Initialise Lyaponov exponent estimator constants ####
-seeked_error_norm = 1e-4
+PAR = Params()
+
+print(hex(id(PAR.pre_factor)), PAR.pre_factor.size)
+print(hex(id(PAR.pre_factor)), PAR.pre_factor.size)
+PAR.sdim = 30
+PAR.initialise_arrays()
+print(hex(id(PAR.pre_factor)), PAR.pre_factor.size)
+print(hex(id(PAR.pre_factor)), PAR.pre_factor.size)
+
+# Define variables not suited for numba jitclass
+u_slice: slice = slice(PAR.bd_size, -PAR.bd_size, 1)
+dtype: type = np.complex128
+
+# # array1 = np.arange(20)
+
+# s1 = s.k_vec_temp
+# print("size", s1.size)
+# s.sdim = 30
+# s2 = s.k_vec_temp
+# print("size", s2.size)
+# print(s1 is s2)
+# s3 = s.k_vec_temp
+# print("size", s3.size)
+# print(s3 is s2)
+# # print(hex(id(s.k_vec_temp)))
+
+# # print(hex(id(s.k_vec_temp)))
+
+# #### Initialise sabra model constants ####
+# epsilon = 0.5
+# lambda_const = 2
+# dt = 1e-7
+# sample_rate = 1 / 1000
+# bd_size = 2
+# n_forcing = 0
+# u0 = 1
+
+# # Define some conversion shortcuts
+# tts = sample_rate / dt
+# stt = dt / sample_rate
+
+# # Define types
+# dtype = np.complex128
+
+# # Define factors to be used in the derivative calculation
+# factor2 = -epsilon / lambda_const
+# factor3 = (1 - epsilon) / lambda_const ** 2
 
 
-def initiate_sdim_arrays(local_sdim: int):
-    """Initiate arrays based on sdim
+# # Define u_slice
+# u_slice = np.s_[bd_size:-bd_size:1]
 
-    Parameters
-    ----------
-    local_sdim : int
-        The dimension of the system
-    """
-
-    #### Initialise sabra model arrays ####
-    global sdim, k_vec_temp, pre_factor, hel_pre_factor, du_array, initial_k_vec
-
-    # Define the global sdim
-    sdim = local_sdim
-    # Define k vector indices
-    k_vec_temp = np.array(
-        [lambda_const ** (n + 1) for n in range(sdim)], dtype=np.float64
-    )
-    pre_factor = 1j * k_vec_temp
-
-    # Helicity pre factor
-    hel_pre_factor = (-1) ** (np.arange(1, sdim + 1)) * k_vec_temp
-
-    # Define du array to store derivative
-    du_array = np.zeros(sdim + 2 * bd_size, dtype=dtype)
-
-    # Calculate initial k and u profile. Put in zeros at the boundaries
-    initial_k_vec = k_vec_temp ** (-1 / 3)
+# #### Initialise Lyaponov exponent estimator constants ####
+# seeked_error_norm = 1e-4
 
 
-def ny_from_ny_n_and_forcing(forcing, ny_n, diff_exponent):
-    """Util func to calculate ny from forcing and ny_n
+# def initiate_sdim_arrays(local_sdim: int):
+#     """Initiate arrays based on sdim
 
-    Parameters
-    ----------
-    forcing : float
-        The applied forcing
-    ny_n : int
-        The shell at which to apply the diffusion
+#     Parameters
+#     ----------
+#     local_sdim : int
+#         The dimension of the system
+#     """
 
-    Returns
-    -------
-    float
-        The viscosity
-    """
-    ny = (forcing / (lambda_const ** (2 * ny_n * (diff_exponent - 2 / 3)))) ** (1 / 2)
+#     #### Initialise sabra model arrays ####
+#     global sdim, k_vec_temp, pre_factor, hel_pre_factor, du_array, initial_k_vec
 
-    return ny
+#     # Define the global sdim
+#     sdim = local_sdim
+#     # Define k vector indices
+#     k_vec_temp = np.array(
+#         [lambda_const ** (n + 1) for n in range(sdim)], dtype=np.float64
+#     )
+#     pre_factor = 1j * k_vec_temp
 
+#     # Helicity pre factor
+#     hel_pre_factor = (-1) ** (np.arange(1, sdim + 1)) * k_vec_temp
 
-def ny_n_from_ny_and_forcing(forcing, ny, diff_exponent):
-    """Util func to calculate ny_n from forcing and ny
+#     # Define du array to store derivative
+#     du_array = np.zeros(sdim + 2 * bd_size, dtype=dtype)
 
-    Parameters
-    ----------
-    forcing : float
-        The applied forcing
-    ny : float
-        The viscosity
-
-    Returns
-    -------
-    int
-        The shell at which to apply the viscosity
-    """
-    ny_n = int(
-        math.log10(forcing / (ny ** 2))
-        / (2 * (diff_exponent - 2 / 3) * math.log10(lambda_const))
-    )
-
-    return ny_n
+#     # Calculate initial k and u profile. Put in zeros at the boundaries
+#     initial_k_vec = k_vec_temp ** (-1 / 3)
--- shell_model_experiments/sabra_model/sabra_model.py
+++ shell_model_experiments/sabra_model/sabra_model.py
@@ -12,8 +12,8 @@ import numpy as np
 from numba import njit, types
 from pyinstrument import Profiler
 from shell_model_experiments.sabra_model.runge_kutta4 import runge_kutta4
-import shell_model_experiments.params as params
-from shell_model_experiments.params.params import *
+from shell_model_experiments.params.params import PAR
+import shell_model_experiments.utils.util_funcs as ut_funcs
 import general.utils.saving.save_data_funcs as g_save
 import general.utils.saving.save_utils as g_save_utils
 import general.utils.argument_parsers as a_parsers
@@ -67,18 +67,18 @@ def run_model(
     # Perform calculations
     for i in range(Nt_local):
         # Save samples for plotting
-        if i % int(1 / sample_rate) == 0:
-            data_out[sample_number, 0] = dt * i + 0j
-            data_out[sample_number, 1:] = u_old[bd_size:-bd_size]
+        if i % int(1 / PAR.sample_rate) == 0:
+            data_out[sample_number, 0] = PAR.dt * i + 0j
+            data_out[sample_number, 1:] = u_old[PAR.bd_size : -PAR.bd_size]
             sample_number += 1
 
         # Solve nonlinear terms + forcing
         u_old = runge_kutta4(
-            y0=u_old, h=dt, du=du_array, forcing=forcing, pre_factor=pre_factor
+            y0=u_old, h=PAR.dt, du=du_array, forcing=forcing, pre_factor=pre_factor
         )
         # Solve linear diffusive term explicitly
-        u_old[bd_size:-bd_size] = u_old[bd_size:-bd_size] * np.exp(
-            -ny * k_vec_temp_local ** diff_exponent * dt
+        u_old[PAR.bd_size : -PAR.bd_size] = u_old[PAR.bd_size : -PAR.bd_size] * np.exp(
+            -ny * k_vec_temp_local ** diff_exponent * PAR.dt
         )
 
     return u_old
@@ -87,66 +87,68 @@ def run_model(
 def main(args=None):
 
     # Define u_old
-    u_old = (u0 * params.initial_k_vec).astype(np.complex128)
-    u_old = np.pad(u_old, pad_width=bd_size, mode="constant")
+    u_old = (PAR.u0 * PAR.initial_k_vec).astype(np.complex128)
+    u_old = np.pad(u_old, pad_width=PAR.bd_size, mode="constant")
 
     # Get number of records
-    args["n_records"] = ceil((args["Nt"]) / int(cfg.GLOBAL_PARAMS.record_max_time / dt))
+    args["n_records"] = ceil(
+        (args["Nt"]) / int(cfg.GLOBAL_PARAMS.record_max_time / PAR.dt)
+    )
 
     # Write ref info file
     g_save.save_reference_info(args)
 
     profiler.start()
     print(
-        f'\nRunning sabra model for {args["Nt"]*dt:.2f}s with a burn-in time'
+        f'\nRunning sabra model for {args["Nt"]*PAR.dt:.2f}s with a burn-in time'
         + f' of {args["burn_in_time"]:.2f}s, i.e. {args["n_records"]:d} records '
         + f"are saved to disk each with {cfg.GLOBAL_PARAMS.record_max_time:.1f}s data\n"
     )
 
     # Burn in the model for the desired burn in time
     data_out = np.zeros(
-        (int(args["burn_in_time"] * tts), params.sdim + 1), dtype=np.complex128
+        (int(args["burn_in_time"] * PAR.tts), PAR.sdim + 1), dtype=np.complex128
     )
 
     print(f'running burn-in phase of {args["burn_in_time"]}s\n')
     u_old = run_model(
         u_old,
-        params.du_array,
+        PAR.du_array,
         data_out,
-        params.k_vec_temp,
-        int(args["burn_in_time"] / dt),
+        PAR.k_vec_temp,
+        int(args["burn_in_time"] / PAR.dt),
         args["ny"],
         args["forcing"],
         args["diff_exponent"],
-        params.pre_factor,
+        PAR.pre_factor,
     )
 
     for ir in range(args["n_records"]):
         # Calculate data out size
         # Set standard size
-        out_array_size = int(cfg.GLOBAL_PARAMS.record_max_time * tts)
+        out_array_size = int(cfg.GLOBAL_PARAMS.record_max_time * PAR.tts)
         # If last record -> adjust if size should not equal default record length
         if ir == (args["n_records"] - 1):
-            if args["Nt"] % int(cfg.GLOBAL_PARAMS.record_max_time / dt) > 0:
+            if args["Nt"] % int(cfg.GLOBAL_PARAMS.record_max_time / PAR.dt) > 0:
                 out_array_size = int(
-                    (args["Nt"] % int(cfg.GLOBAL_PARAMS.record_max_time / dt))
-                    * sample_rate
+                    (args["Nt"] % int(cfg.GLOBAL_PARAMS.record_max_time / PAR.dt))
+                    * PAR.sample_rate
                 )
 
-        data_out = np.zeros((out_array_size, params.sdim + 1), dtype=np.complex128)
+        data_out = np.zeros((out_array_size, PAR.sdim + 1), dtype=np.complex128)
 
         # Run model
         print(f'running record {ir + 1}/{args["n_records"]}')
         u_old = run_model(
             u_old,
-            params.du_array,
+            PAR.du_array,
             data_out,
-            params.k_vec_temp,
-            int(out_array_size / sample_rate),
+            PAR.k_vec_temp,
+            int(out_array_size / PAR.sample_rate),
             args["ny"],
             args["forcing"],
             args["diff_exponent"],
-            params.pre_factor,
+            PAR.pre_factor,
         )
 
         # Add record_id to datafile header
@@ -171,11 +173,11 @@ if __name__ == "__main__":
     args = stand_arg_setup.args
 
     # Initiate variables
-    initiate_sdim_arrays(args["sdim"])
-    args["ny"] = ny_from_ny_n_and_forcing(
+    PAR.initiate_sdim_arrays(args["sdim"])
+    args["ny"] = ut_funcs.ny_from_ny_n_and_forcing(
         args["forcing"], args["ny_n"], args["diff_exponent"]
     )
 
-    args["Nt"] = int(args["time_to_run"] / dt)
+    args["Nt"] = int(args["time_to_run"] / PAR.dt)
 
     main(args=args)
