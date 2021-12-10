Investigated why we get so large exponential growth rates for the shell model. It seems like the calculation is correct, and the reason is that we also have a very small time step in the shell model -> high growth rates with small time step gives the perturbation growth we observe. Fixed a small error with the sv calculations - seeked error norm have to be 1 (at least for the l63 model) when we calculate the vectors. Made proper sorting of the perturbation files based on the profile and the run_in_profile values from the header_dict. Made proper truncation of the number of perturbation files imported to import the n_files first files after sorting.

general/analyses/analyse_data.py
@@ -102,13 +102,15 @@ def analyse_mean_exp_growth_rate_vs_time(
 
     for i in range(1, n_datapoints):
         # Instantaneous exponential growth rate
-        # growth_rates[i - 1, :] = (1 / (params.stt)) * np.log(
-        #     error_norm_vs_time[i, :] / error_norm_vs_time[i - 1, :]
-        # )
-        # 'Averaged' (i.e. over some interval i*dt)
-        growth_rates[i - 1, :] = (1 / (i * params.stt)) * np.log(
-            error_norm_vs_time[i, :] / error_norm_vs_time[0, :]
+        growth_rates[i - 1, :] = (
+            1
+            / params.stt
+            * np.log(error_norm_vs_time[i, :] / error_norm_vs_time[i - 1, :])
         )
+        # 'Averaged' (i.e. over some interval i*dt)
+        # growth_rates[i - 1, :] = (1 / (i * params.stt)) * np.log(
+        #     error_norm_vs_time[i, :] / error_norm_vs_time[0, :]
+        # )
 
     mean_growth_rate = np.mean(growth_rates, axis=1)
 

general/plotting/plot_data.py
@@ -15,12 +15,16 @@ import config as cfg
 if cfg.MODEL == Models.SHELL_MODEL:
     from shell_model_experiments.params.params import ParamsStructType
     from shell_model_experiments.params.params import PAR as PAR_SH
+    import shell_model_experiments.utils.special_params as sh_sparams
 
     params = PAR_SH
+    sparams = sh_sparams
 elif cfg.MODEL == Models.LORENTZ63:
     import lorentz63_experiments.params.params as l63_params
+    import lorentz63_experiments.params.special_params as l63_sparams
 
     params = l63_params
+    sparams = l63_sparams
 
 
 def plot_exp_growth_rate_vs_time(
@@ -56,22 +60,23 @@ def plot_exp_growth_rate_vs_time(
     ) = g_import.import_perturbation_velocities(args, search_pattern="*perturb*.csv")
 
     # Define time array
+    # -1 since growth rate is a rate between differences (see functino
+    # analyse_mean_exp_growth_rate_vs_time)
     time_array = np.linspace(
         0,
         header_dicts[0]["time_to_run"],
-        int(header_dicts[0]["time_to_run"] * params.tts) + args["endpoint"] * 1,
+        int(header_dicts[0]["time_to_run"] * params.tts) + args["endpoint"] * 1 - 1,
         dtype=np.float64,
         endpoint=args["endpoint"],
     )
 
     n_runs_per_profile = len(perturb_time_pos_list)
 
-    # Analyse error norm and mean exponential growth rate
+    # Analyse mean exponential growth rate
     (
         error_norm_vs_time,
         error_norm_mean_vs_time,
     ) = g_a_data.analyse_error_norm_vs_time(u_stores, args=args)
-
     mean_growth_rate = g_a_data.analyse_mean_exp_growth_rate_vs_time(
         error_norm_vs_time, args=args
     )
@@ -249,11 +254,9 @@ def plot_error_norm_vs_time(
         ]:
             axes.legend(perturb_time_pos_list_legend)
         elif cfg.LICENCE == EXP.SINGULAR_VECTORS:
-            lines: list = list(axes.get_lines())
             for i, header_dict in enumerate(header_dicts):
                 if "run_in_profile" in header_dict:
                     lines[i].set_label(f"sv{int(header_dict['run_in_profile'])}")
-                    # lines[i].set_color(cmap_list[int(header_dict["run_in_profile"])])
                     if i + 1 == n_runs_per_profile:
                         break
             axes.legend()

general/runners/singular_vector_lanczos_runner.py
@@ -94,8 +94,11 @@ def main(args: dict, exp_setup: dict = None):
     copy_args_atl = copy.deepcopy(args)
 
     # Set error norm to 1
-    # _temp_seeked_error_norm = copy.deepcopy(params.seeked_error_norm)
-    # params.seeked_error_norm = 1
+    _temp_seeked_error_norm = copy.deepcopy(params.seeked_error_norm)
+    if cfg.MODEL == Models.SHELL_MODEL:
+        sh_utils.set_params(params, parameter="seeked_error_norm", value=1)
+    elif cfg.MODEL == Models.LORENTZ63:
+        params.seeked_error_norm = 1
 
     # Calculate the desired number of units
     for i in range(
@@ -126,10 +129,10 @@ def main(args: dict, exp_setup: dict = None):
             lanczos_outarray = None
 
             # Initiate the Lanczos arrays and algorithm
-            propagated_vector: np.ndarray((params.sdim, 1)) = np.empty(
+            propagated_vector: np.ndarray((params.sdim, 1)) = np.zeros(
                 (params.sdim, 1), dtype=sparams.dtype
             )
-            input_vector: np.ndarray((params.sdim, 1)) = np.empty(
+            input_vector: np.ndarray((params.sdim, 1)) = np.zeros(
                 (params.sdim, 1), dtype=sparams.dtype
             )
             lanczos_iterator = pt_utils.lanczos_vector_algorithm(
@@ -251,7 +254,12 @@ def main(args: dict, exp_setup: dict = None):
     g_save.save_exp_info(exp_setup, args)
 
     # Reset seeked error norm
-    # params.seeked_error_norm = _temp_seeked_error_norm
+    if cfg.MODEL == Models.SHELL_MODEL:
+        sh_utils.set_params(
+            params, parameter="seeked_error_norm", value=_temp_seeked_error_norm
+        )
+    elif cfg.MODEL == Models.LORENTZ63:
+        params.seeked_error_norm = _temp_seeked_error_norm
 
     if args["erda_run"]:
         path = pl.Path(args["datapath"], exp_setup["folder_name"])

general/utils/importing/import_data_funcs.py
@@ -158,34 +158,35 @@ def imported_sorted_perturbation_info(folder_name, args, search_pattern="*.csv")
 
         perturb_header_dicts.append(perturb_header_dict)
 
-    # Get sort index
-    ascending_perturb_pos_index = np.argsort(perturb_time_pos_list)
+    # Sort according to profile and run_in_profile if the keys are present
+    if "profile" in perturb_header_dicts[0]:
+        (
+            perturb_file_names,
+            ascending_sort_index,
+        ) = g_utils.sort_paths_according_to_header_dicts(
+            perturb_file_names, ["profile", "run_in_profile"]
+        )
+    else:
+        # Get sort index
+        ascending_sort_index = np.argsort(perturb_time_pos_list)
+        # Sort perturb file names
+        perturb_file_names = [perturb_file_names[i] for i in ascending_sort_index]
 
     # Sort arrays/lists
     perturb_time_pos_list = np.array(
-        [
-            perturb_time_pos_list[i]
-            for i in ascending_perturb_pos_index
-            if (i + 1) <= args["n_files"]
-        ]
+        [perturb_time_pos_list[i] for i in ascending_sort_index]
     )
     perturb_time_pos_list_legend = np.array(
-        [
-            perturb_time_pos_list_legend[i]
-            for i in ascending_perturb_pos_index
-            if (i + 1) <= args["n_files"]
-        ]
+        [perturb_time_pos_list_legend[i] for i in ascending_sort_index]
     )
-    perturb_header_dicts = [
-        perturb_header_dicts[i]
-        for i in ascending_perturb_pos_index
-        if (i + 1) <= args["n_files"]
-    ]
-    perturb_file_names = [
-        perturb_file_names[i]
-        for i in ascending_perturb_pos_index
-        if (i + 1) <= args["n_files"]
-    ]
+    perturb_header_dicts = [perturb_header_dicts[i] for i in ascending_sort_index]
+
+    # Truncate at n_files
+    if args["n_files"] < np.inf:
+        perturb_file_names = perturb_file_names[: args["n_files"]]
+        perturb_time_pos_list = perturb_time_pos_list[: args["n_files"]]
+        perturb_time_pos_list_legend = perturb_time_pos_list_legend[: args["n_files"]]
+        perturb_header_dicts = perturb_header_dicts[: args["n_files"]]
 
     return (
         perturb_time_pos_list,