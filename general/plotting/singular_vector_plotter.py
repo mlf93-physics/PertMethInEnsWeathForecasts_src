Plot edits. Added average plot of the orthogonality between svs. Fixed error with an imported vector unit being set to only the real part - for shell model, we use complex svs, so it introduced wrong svs used in perturbation runner. Solved error when running n_lanczos_iterations > 1. Added test run to the compare_pert_experiment_setup.json of the shell model. Added experiment to the sv exp setup of the shell model.

general/plotting/singular_vector_plotter.py
@@ -27,6 +27,7 @@ import general.utils.util_funcs as g_utils
 import general.utils.plot_utils as g_plt_utils
 import general.utils.user_interface as g_ui
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mpl_ticker
 import seaborn as sb
 import numpy as np
 from general.params.model_licences import Models
@@ -76,9 +77,9 @@ def plot_s_values(args, axes: plt.Axes = None, plot_args=[]):
     for i, header_dict in enumerate(header_dicts):
         labels.append(f"Start time: {header_dict['val_pos']*params.stt:.2f}")
 
-    real_lines = axes[0].plot(singular_values.T.real)
+    real_lines = axes[0].plot(singular_values.T.real, "k", alpha=0.3)
     colors = [line.get_color() for line in real_lines]
-    imag_lines = axes[0].plot(singular_values.T.imag, linestyle="--")
+    imag_lines = axes[0].plot(singular_values.T.imag, linestyle="--", alpha=0.3)
 
     for i, line in enumerate(imag_lines):
         line.set_color(colors[i])
@@ -185,12 +186,13 @@ def plot_s_vectors_average(args, axes: plt.Axes = None, plot_args: list = []):
 
     mean_abs_singular_vector_units = np.mean(np.abs(singular_vector_units), axis=0)
 
-    sb.heatmap(
-        mean_abs_singular_vector_units.T,
-        ax=axes,
-    )
+    sb.heatmap(mean_abs_singular_vector_units.T, ax=axes, cbar_kws={"pad": 0.1})
 
     axes.invert_yaxis()
+    axes.invert_xaxis()
+    plt.xticks(rotation=0)
+    axes.yaxis.tick_right()
+    axes.yaxis.set_label_position("right")
     axes.set_xlabel("SV index")
     axes.set_ylabel("Shell index")
 
@@ -203,7 +205,7 @@ def plot_s_vectors_average(args, axes: plt.Axes = None, plot_args: list = []):
     axes.set_title(s_vector_title)
 
 
-def plot_s_vector_orthogonality(args, axes=None):
+def plot_s_vector_ortho(args, axes=None):
 
     # Import breed vectors
     (
@@ -271,6 +273,62 @@ def plot_s_vector_orthogonality(args, axes=None):
     fig.tight_layout(rect=[0, 0, 0.9, 1])
 
 
+def plot_s_vector_ortho_average(args, axes=None):
+
+    # Import breed vectors
+    (
+        singular_vector_units,
+        singular_values,
+        _,
+        _,
+        header_dicts,
+    ) = pt_import.import_perturb_vectors(
+        args, raw_perturbations=True, dtype=sparams.dtype
+    )
+
+    # Normalize
+    singular_vector_units = g_utils.normalize_array(
+        singular_vector_units, norm_value=1, axis=2
+    )
+
+    # Prepare axes
+    if axes is None:
+        fig, axes = plt.subplots(ncols=1, nrows=1)
+
+    orthogonality_matrix = 0
+    for i in range(args["n_profiles"]):
+        orthogonality_matrix += g_plt_anal.orthogonality_of_vectors(
+            singular_vector_units[i, :, :]
+        )
+
+    orthogonality_matrix /= args["n_profiles"]
+
+    sb.heatmap(
+        orthogonality_matrix,
+        ax=axes,
+        cmap="Reds",
+        vmin=0,
+        vmax=1,
+        # annot=True,
+        fmt=".1f",
+        annot_kws={"fontsize": 8},
+        cbar_kws=dict(use_gridspec=True, label="Orthogonality"),
+    )
+
+    axes.set_title(f'Val. pos. {header_dicts[i]["val_pos"]*params.stt:.2f}')
+
+    # Generate title
+    s_vector_title = g_plt_utils.generate_title(
+        args,
+        header_dict=header_dicts[0],
+        title_header="Averaged orthogonality between SVs | shell model \n",
+    )
+    fig.suptitle(s_vector_title)
+    fig.supxlabel("SV index")
+    fig.supylabel("SV index")
+    fig.tight_layout(rect=[0, 0, 0.9, 1])
+
+
 def plot_sv_error_norm(args):
 
     axes = plt.axes()
@@ -306,13 +364,15 @@ if __name__ == "__main__":
     profiler.start()
 
     if "s_values" in args["plot_type"]:
-        plot_s_values(args, plot_args=[])
+        plot_s_values(args, plot_args=["normalize"])
     elif "s_vectors" in args["plot_type"]:
         plot_s_vectors_units(args)
     elif "s_vectors_average" in args["plot_type"]:
         plot_s_vectors_average(args)
-    elif "s_vector_orthogonality" in args["plot_type"]:
-        plot_s_vector_orthogonality(args)
+    elif "s_vector_ortho" in args["plot_type"]:
+        plot_s_vector_ortho(args)
+    elif "s_vector_ortho_average" in args["plot_type"]:
+        plot_s_vector_ortho_average(args)
     elif "sv_error_norm" in args["plot_type"]:
         plot_sv_error_norm(args)
     else:

general/utils/running/sv_runner_utils.py
@@ -46,11 +46,6 @@ def sv_generator(
     input_vector: np.ndarray((params.sdim, 1)) = np.zeros(
         (params.sdim, 1), dtype=sparams.dtype
     )
-    lanczos_iterator = pt_utils.lanczos_vector_algorithm(
-        propagated_vector=propagated_vector,
-        input_vector_j=input_vector,
-        n_iterations=exp_setup["n_vectors"],
-    )
 
     sv_matrix = np.zeros(
         (params.sdim, exp_setup["n_vectors"]),
@@ -76,12 +71,16 @@ def sv_generator(
         lorentz_matrix = ut_funcs.setup_lorentz_matrix(copy_args)
         jacobian_matrix = l63_nm_estimator.init_jacobian(copy_args)
 
-    # Start out by running on u_old, but hereafter lanczos_outarray will be
-    # updated by the lanczos_iterator
-    lanczos_outarray = u_old
-
     # Average over multiple iterations of the lanczos algorithm
     for _ in range(exp_setup["n_lanczos_iterations"]):
+        # Start out by running on u_old, but hereafter lanczos_outarray will be
+        # updated by the lanczos_iterator
+        lanczos_outarray = u_old
+        lanczos_iterator = pt_utils.lanczos_vector_algorithm(
+            propagated_vector=propagated_vector,
+            input_vector_j=input_vector,
+            n_iterations=exp_setup["n_vectors"],
+        )
         # Calculate the desired number of SVs
         for _ in range(exp_setup["n_vectors"]):
             # Run specified number of model iterations