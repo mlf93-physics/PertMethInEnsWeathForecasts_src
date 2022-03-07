Fixed bug in projectibility plot. Fixed bug in FSV calculation for the L63 model. Added FSV calculation to the shell model without testing it.

general/plotting/plot_comparisons.py
@@ -246,7 +246,9 @@ def plt_vec_compared_to_lv(args, axes: plt.Axes = None):
             args,
             vector=vector,
             raw_perturbations=not vector == "bv",
-            dtype=np.complex128 if vector == "sv" else sparams.dtype,
+            dtype=np.complex128
+            if "sv" in vector
+            else sparams.dtype,  # Both sv's and fsv's
             retrieve_header_key=retrieve_header_key,
             zero_iw_only=(vector == "lv" or vector == "alv")
             and "fsv" not in save_vectors,
@@ -364,22 +366,25 @@ def plt_vec_compared_to_lv(args, axes: plt.Axes = None):
             line_objs = zip(*vector_vs_lv_lines)
 
             # Set labels and colors
-            for i, line_objs in enumerate(line_objs):
+            for i, line_obj in enumerate(line_objs):
                 if "lv" in save_vectors:
-                    line_objs[0].set_label(
+                    line_obj[0].set_label(
                         f"{vector.upper()} vs LV{i}",
                     )
-                if "alv" in save_vectors and len(line_objs) == 1:
-                    line_objs[0].set_label(
-                        f"{vector.upper()} vs ALV{i}",
-                    )
-                else:
-                    line_objs[1].set_label(
-                        f"{vector.upper()} vs ALV{i}",
-                    )
-                    line_objs[1].set_color(line_objs[0].get_color())
+                if "alv" in save_vectors:
+                    if len(line_obj) == 1:
+                        line_obj[0].set_label(
+                            f"{vector.upper()} vs ALV{i}",
+                        )
+                        # Reset linestyle
+                        line_obj[0].set_linestyle("solid")
+                    else:
+                        line_obj[1].set_label(
+                            f"{vector.upper()} vs ALV{i}",
+                        )
+                        line_obj[1].set_color(line_obj[0].get_color())
 
-        # Plot SVs vs LVs
+        # Plot SVs/FSVs vs LVs
         if "sv" in vector:
             for j in range(args["n_lvs_to_compare"]):
                 vector_vs_lines: list = []
@@ -403,22 +408,24 @@ def plt_vec_compared_to_lv(args, axes: plt.Axes = None):
                 vector_string: str = (
                     ("I" + vector.upper()) if vector == "sv" else vector.upper()
                 )
-                for i, line_objs in enumerate(line_objs):
+                for i, line_obj in enumerate(line_objs):
+
                     if "lv" in save_vectors:
-                        line_objs[0].set_label(
+                        line_obj[0].set_label(
                             f"{vector_string}{i} vs LV{j}",
                         )
-                    if "alv" in save_vectors and len(line_objs) == 1:
-                        line_objs[0].set_label(
-                            f"{vector_string}{i} vs ALV{j}",
-                        )
-                        # Reset linestyle
-                        line_objs[0].set_linestyle("solid")
-                    else:
-                        line_objs[1].set_label(
-                            f"{vector_string}{i} vs ALV{j}",
-                        )
-                        line_objs[1].set_color(line_objs[0].get_color())
+                    if "alv" in save_vectors:
+                        if len(line_obj) == 1:
+                            line_obj[0].set_label(
+                                f"{vector_string}{i} vs ALV{j}",
+                            )
+                            # Reset linestyle
+                            line_obj[0].set_linestyle("solid")
+                        else:
+                            line_obj[1].set_label(
+                                f"{vector_string}{i} vs ALV{j}",
+                            )
+                            line_obj[1].set_color(line_obj[0].get_color())
 
     axes.set_xlabel("Integration window (IW)")
     axes.set_ylabel("Absolute projectibility")

general/utils/running/sv_runner_utils.py
@@ -14,6 +14,7 @@ if cfg.MODEL == Models.SHELL_MODEL:
     import shell_model_experiments.utils.util_funcs as ut_funcs
     from shell_model_experiments.sabra_model.sabra_model_combinations import (
         sh_tl_atl_model,
+        sh_atl_tl_model,
     )
 
     params = PAR_SH
@@ -87,22 +88,40 @@ def sv_generator(
             # Run specified number of model iterations
             for _ in range(exp_setup["n_model_iterations"]):
                 if cfg.MODEL == Models.SHELL_MODEL:
-                    _, u_atl_out, _ = sh_tl_atl_model(
-                        np.pad(
-                            lanczos_vector_matrix[:, i],
-                            pad_width=params.bd_size,  # , params.bd_size), (0, 0)),
-                            mode="constant",
-                        ),
-                        u_ref,
-                        data_out,
-                        copy_args,
-                        J_matrix,
-                        diagonal0,
-                        diagonal1,
-                        diagonal2,
-                        diagonal_1,
-                        diagonal_2,
-                    )
+                    if cfg.LICENCE == EXP.SINGULAR_VECTORS:
+                        _, u_atl_out, _ = sh_tl_atl_model(
+                            np.pad(
+                                lanczos_vector_matrix[:, i],
+                                pad_width=params.bd_size,  # , params.bd_size), (0, 0)),
+                                mode="constant",
+                            ),
+                            u_ref,
+                            data_out,
+                            copy_args,
+                            J_matrix,
+                            diagonal0,
+                            diagonal1,
+                            diagonal2,
+                            diagonal_1,
+                            diagonal_2,
+                        )
+                    elif cfg.LICENCE == EXP.FINAL_SINGULAR_VECTORS:
+                        _, u_atl_out, _ = sh_atl_tl_model(
+                            np.pad(
+                                lanczos_vector_matrix[:, i],
+                                pad_width=params.bd_size,  # , params.bd_size), (0, 0)),
+                                mode="constant",
+                            ),
+                            u_ref,
+                            data_out,
+                            copy_args,
+                            J_matrix,
+                            diagonal0,
+                            diagonal1,
+                            diagonal2,
+                            diagonal_1,
+                            diagonal_2,
+                        )
                 elif cfg.MODEL == Models.LORENTZ63:
                     if cfg.LICENCE == EXP.SINGULAR_VECTORS:
                         # Run TL model followed by ATL model